8d5ebb461252668e239a60f2fc33fea97d588a98
Humberto Borba <humberos@gmail.com>
2014-02-14 14:30:18 -0200

    p1-common: audio: add 48000 sample rating support
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: Ib4f376a7390e80c18bd9013956e1e19947ed16d3

--- libaudio/Android.mk
+++ libaudio/Android.mk
@@ -25,6 +25,7 @@ LOCAL_MODULE := audio.primary.s5pc110
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_STATIC_LIBRARIES:= libmedia_helper
 LOCAL_SHARED_LIBRARIES:= \
+	liblog \
 	libutils \
 	libhardware_legacy \
 	libtinyalsa \
@@ -33,7 +34,7 @@ LOCAL_SHARED_LIBRARIES:= \
 LOCAL_WHOLE_STATIC_LIBRARIES := libaudiohw_legacy
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_SHARED_LIBRARIES += libdl liblog
+LOCAL_SHARED_LIBRARIES += libdl
 LOCAL_C_INCLUDES += \
 	external/tinyalsa/include \
 	$(call include-path-for, audio-effects) \
