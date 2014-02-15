33207ac013bec14885b86bed79a5741df8f08206
Humberto Borba <humberos@gmail.com>
2014-02-15 19:43:33 -0200

    p1-common: init: use aosp recovery script
    removed sdcard[0|1] reference
    aosp recovery script will find package name automagically
    it will find the package in cache, sdcard0 or sdcard1
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I88fd8b031e286066824f676f9747e5f0987bdbc6

--- updater.sh
+++ updater.sh
@@ -48,9 +48,11 @@ fix_package_location() {
     # Remove leading /mnt
     PACKAGE_LOCATION=${PACKAGE_LOCATION#/mnt}
     # Convert to modern sdcard path
-    PACKAGE_LOCATION=`echo $PACKAGE_LOCATION | busybox sed -e "s|^/sdcard|/storage/sdcard0|"`
-    PACKAGE_LOCATION=`echo $PACKAGE_LOCATION | busybox sed -e "s|^/emmc|/storage/sdcard1|"`
-    PACKAGE_LOCATION=`echo $PACKAGE_LOCATION | busybox sed -e "s|^/external_sd|/storage/sdcard1|"`
+    PACKAGE_LOCATION=`echo $PACKAGE_LOCATION | busybox sed -e "s|^/sdcard/||"`
+    PACKAGE_LOCATION=`echo $PACKAGE_LOCATION | busybox sed -e "s|^/emmc/||"`
+    PACKAGE_LOCATION=`echo $PACKAGE_LOCATION | busybox sed -e "s|^/external_sd/||"`
+    PACKAGE_LOCATION=`echo $PACKAGE_LOCATION | busybox sed -e "s|^/storage/sdcard1/||"`
+    PACKAGE_LOCATION=`echo $PACKAGE_LOCATION | busybox sed -e "s|^/storage/sdcard1/||"`
     echo $PACKAGE_LOCATION
 }
 
