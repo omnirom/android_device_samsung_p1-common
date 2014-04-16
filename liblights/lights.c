f86adc2dda5f8727f989f6047ea56daaababf740
Humberto Borba <humberos@gmail.com>
2014-04-15 21:09:57 -0300

    p1-common: add button led brightness control to liblight
    button led brightness have 3 levels
    
    1. (0.5mA)
    2. (1.0mA)
    3. (2.0mA)
    
    lcd lib light have 255 as max value
    
    so, 255 / 3 = 85
    
    changed light value  => changed button brightness value
    
    from   0 to  84 => 1 (0.5mA)
    from  85 to 169 => 2 (1.0mA)
    from 170 to 255 => 3 (2.0mA)
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I2ea0f46cfef3cc4d5474255060c785b604c600f5

--- liblights/lights.c
+++ liblights/lights.c
@@ -34,6 +34,9 @@ static pthread_mutex_t g_lock = PTHREAD_MUTEX_INITIALIZER;
 char const*const LCD_BACKLIGHT_FILE = "/sys/class/backlight/s5p_bl/brightness";
 char const*const BUTTON_FILE = "/sys/class/leds/button-backlight/brightness";
 
+char const*const BUTTON_LED_BRIGHTNESS_FILE =
+	"/sys/devices/platform/s3c2440-i2c.2/i2c-2/2-004a/key_led_brightness";
+
 static int write_int(char const *path, int value)
 {
 	int fd;
@@ -101,11 +104,22 @@ static int set_light_buttons (struct light_device_t* dev,
 static int set_light_backlight(struct light_device_t* dev,
 		struct light_state_t const* state) {
 	int err = 0;
+	int button_led_brightness = 1;
 	int brightness = rgb_to_brightness(state);
 	ALOGV("%s brightness=%d color=0x%08x",
 		__func__,brightness, state->color);
 	pthread_mutex_lock(&g_lock);
 	err = write_int(LCD_BACKLIGHT_FILE, brightness);
+
+	// update button led brightness
+	button_led_brightness = 1;
+	if (brightness >= 170) {
+		button_led_brightness = 3;
+	} else if(brightness >= 85 && brightness < 170) {
+		button_led_brightness = 2;
+	}
+	err = write_int (BUTTON_LED_BRIGHTNESS_FILE, button_led_brightness);
+
 	// if LCD backlight is on then button backlight should be on too
 	// Samsung has some annoying code that turns on buttons light only when touchscreen is pressed
 	// and it turns off automatically afterwards, leaving user hunting for buttons in the dark
