7ecb521a5b3247a68823bab06135cdd41487b93c
Humberto Borba <humberos@gmail.com>
2014-04-30 18:44:53 +0200

    Revert "p1-common: add lvm support [2/3]"
    just for /data space
    I will revert all LVM partitions.
    
    This reverts commit 4c7147b326afd7f758220788154494f86046d9af.
    
    Change-Id: I2ad7019cc031f8bf5bf62b2d35a8a9a579403620

--- updater.sh
+++ updater.sh
@@ -20,8 +20,6 @@
 # Combined GSM & CDMA version
 #
 
-SYSTEM_SIZE='851443712' # 800M
-
 check_mount() {
     local MOUNT_POINT=`busybox readlink $1`
     if ! busybox test -n "$MOUNT_POINT" ; then
@@ -45,53 +43,21 @@ set_log() {
     exec >> $1 2>&1
 }
 
-warn_repartition() {
-    if ! busybox test -e /.accept_wipe ; then
-        busybox touch /.accept_wipe
-        ui_print ""
-        ui_print "============================================"
-        ui_print "ATTENTION"
-        ui_print ""
-        ui_print "This VERSION uses an incompatible partition layout"
-        ui_print "Your /data will be wiped upon installation"
-        ui_print "So, make your backups (if you want) and then just"
-        ui_print "Run this update.zip again to confirm install"
-        ui_print ""
-        ui_print "ATTENTION"
-        ui_print "============================================"
-        ui_print ""
-        exit 9
-    fi
-    busybox rm /.accept_wipe
-}
-
-format_partitions() {
-    /lvm/sbin/lvm lvcreate -L ${SYSTEM_SIZE}B -n system lvpool
-    /lvm/sbin/lvm lvcreate -l 100%FREE -n userdata lvpool
-
-    # format data (/system will be formatted by updater-script)
-    /tmp/make_ext4fs -b 4096 -g 32768 -i 8192 -I 256 -l -16384 -a /data /dev/lvpool/userdata
-
-    # unmount and format datadata
-    busybox umount -l /datadata
-    /tmp/erase_image datadata
-}
-
 fix_package_location() {
     local PACKAGE_LOCATION=$1
-    # Remove leading /mnt for Samsung recovery
+    # Remove leading /mnt
     PACKAGE_LOCATION=${PACKAGE_LOCATION#/mnt}
     # Convert to modern sdcard path
     PACKAGE_LOCATION=`echo $PACKAGE_LOCATION | busybox sed -e "s|^/sdcard/||"`
     PACKAGE_LOCATION=`echo $PACKAGE_LOCATION | busybox sed -e "s|^/emmc/||"`
     PACKAGE_LOCATION=`echo $PACKAGE_LOCATION | busybox sed -e "s|^/external_sd/||"`
-    PACKAGE_LOCATION=`echo $PACKAGE_LOCATION | busybox sed -e "s|^/storage/sdcard0/||"`
+    PACKAGE_LOCATION=`echo $PACKAGE_LOCATION | busybox sed -e "s|^/storage/sdcard1/||"`
     PACKAGE_LOCATION=`echo $PACKAGE_LOCATION | busybox sed -e "s|^/storage/sdcard1/||"`
     echo $PACKAGE_LOCATION
 }
 
 # ui_print by Chainfire
-OUTFD=$(busybox ps | busybox grep -v "grep" | busybox grep -o -E "/tmp/updater .*" | busybox cut -d " " -f 3);
+OUTFD=$(busybox ps | busybox grep -v "grep" | busybox grep -o -E "update_binary(.*)" | busybox cut -d " " -f 3);
 ui_print() {
   if [ $OUTFD != "" ]; then
     echo "ui_print ${1} " 1>&$OUTFD;
@@ -109,14 +75,20 @@ if busybox test "$1" = cdma ; then
     # CDMA mode
     IS_GSM='busybox false'
     SD_PART='/dev/block/mmcblk1p1'
-    MMC_PART='/dev/block/mmcblk0p1 /dev/block/mmcblk0p2'
     MTD_SIZE='490733568'
 else
     # GSM mode
     IS_GSM='busybox true'
     SD_PART='/dev/block/mmcblk0p1'
-    MMC_PART='/dev/block/mmcblk0p2'
-    MTD_SIZE='442499072'
+    MTD_SIZE='454557696'
+
+    EFS_PART=`busybox grep efs /proc/mtd | busybox awk '{print $1}'`
+    EFS_PART=`busybox echo "$EFS_PART" | busybox sed 's/://g'`
+    EFS_PART=`busybox echo "$EFS_PART" | busybox sed 's/mtd/mtdblock/g'`
+
+    RADIO_PART=`busybox grep radio /proc/mtd | busybox awk '{print $1}'`
+    RADIO_PART=`busybox echo "$RADIO_PART" | busybox sed 's/://g'`
+    RADIO_PART=`busybox echo "$RADIO_PART" | busybox sed 's/mtd/mtdblock/g'`
 fi
 
 # Check if this is a CDMA device with no eMMC
@@ -173,7 +145,7 @@ if busybox test -e /dev/block/bml7 ; then
     exit 0
 
 elif busybox test `busybox cat /sys/class/mtd/mtd2/size` != "$MTD_SIZE" || \
-    busybox test `busybox cat /sys/class/mtd/mtd2/name` != "datadata" ; then
+    busybox test `busybox cat /sys/class/mtd/mtd2/name` != "system" ; then
     # we're running on a mtd (old) device
 
     # make sure sdcard is mounted
@@ -182,19 +154,37 @@ elif busybox test `busybox cat /sys/class/mtd/mtd2/size` != "$MTD_SIZE" || \
     # everything is logged into /sdcard/omni_mtd_old.log
     set_log /sdcard/omni_mtd_old.log
 
-    warn_repartition
+    if ! busybox test -e /cache/.accept_wipe ; then
+        busybox touch /cache/.accept_wipe
+        ui_print
+        ui_print "============================================"
+        ui_print "This ROM uses an incompatible partition layout"
+        ui_print "Your /data will be wiped upon installation"
+        ui_print "Run this update.zip again to confirm install"
+        ui_print "============================================"
+        ui_print
+        exit 9
+    fi
+    busybox rm /cache/.accept_wipe
 
     # write the package path to sdcard omni.cfg
     if busybox test -n "$UPDATE_PACKAGE" ; then
         busybox echo `fix_package_location $UPDATE_PACKAGE` > /sdcard/omni.cfg
     fi
 
-    # inform the script that this is an old mtd upgrade
-    busybox echo 1 > /sdcard/omni.mtdupd
+    if $IS_GSM ; then
+        # make sure efs is mounted
+        check_mount /efs /dev/block/$EFS_PART yaffs2
 
-    # clear datadata
-    busybox umount -l /datadata
-    /tmp/erase_image datadata
+        # create a backup of efs
+        if busybox test -e /sdcard/backup/efs ; then
+            busybox mv /sdcard/backup/efs /sdcard/backup/efs-$$
+        fi
+        busybox rm -rf /sdcard/backup/efs
+
+        busybox mkdir -p /sdcard/backup/efs
+        busybox cp -R /efs/ /sdcard/backup
+    fi
 
     # write new kernel to boot partition
     /tmp/bml_over_mtd.sh boot 72 reservoir 2004 /tmp/boot.img
@@ -218,31 +208,14 @@ elif busybox test -e /dev/block/mtdblock0 ; then
     # everything is logged into /sdcard/omni.log
     set_log /sdcard/omni_mtd.log
 
-    # unmount system and data (recovery seems to expect system to be unmounted)
-    busybox umount -l /system
-    busybox umount -l /data
-
-    # Resize partitions
-    # (For first install, this will get skipped because device doesn't exist)
-    if ! busybox test -e /dev/mapper/lvpool-system ; then
-        warn_repartition
-        format_partitions
-    else
-        if busybox test `busybox blockdev --getsize64 /dev/mapper/lvpool-system` -lt $SYSTEM_SIZE ; then
-            warn_repartition
-            /lvm/sbin/lvm lvremove -f lvpool
-            format_partitions
-        fi
-    fi
-
     if $IS_GSM ; then
         # create mountpoint for radio partition
         busybox mkdir -p /radio
 
         # make sure radio partition is mounted
         if ! busybox grep -q /radio /proc/mounts ; then
-            busybox umount -l /dev/block/mtdblock5
-            if ! busybox mount -t yaffs2 /dev/block/mtdblock5 /radio ; then
+            busybox umount -l /dev/block/$RADIO_PART
+            if ! busybox mount -t yaffs2 /dev/block/$RADIO_PART /radio ; then
                 busybox echo "Cannot mount radio partition."
                 exit 5
             fi
@@ -250,9 +223,9 @@ elif busybox test -e /dev/block/mtdblock0 ; then
 
         # if modem.bin doesn't exist on radio partition, format the partition and copy it
         if ! busybox test -e /radio/modem.bin ; then
-            busybox umount -l /dev/block/mtdblock5
+            busybox umount -l /dev/block/$RADIO_PART
             /tmp/erase_image radio
-            if ! busybox mount -t yaffs2 /dev/block/mtdblock5 /radio ; then
+            if ! busybox mount -t yaffs2 /dev/block/$RADIO_PART /radio ; then
                 busybox echo "Cannot copy modem.bin to radio partition."
                 exit 5
             else
@@ -261,7 +234,7 @@ elif busybox test -e /dev/block/mtdblock0 ; then
         fi
 
         # unmount radio partition
-        busybox umount -l /dev/block/mtdblock5
+        busybox umount -l /radio
     fi
 
     if ! busybox test -e /sdcard/omni.cfg ; then
@@ -271,46 +244,38 @@ elif busybox test -e /dev/block/mtdblock0 ; then
         # flash boot image
         /tmp/bml_over_mtd.sh boot 72 reservoir 2004 /tmp/boot.img
 
-        if ! $IS_GSM ; then
-            /tmp/bml_over_mtd.sh recovery 102 reservoir 2004 /tmp/recovery_kernel
-        fi
+        # unmount system (recovery seems to expect system to be unmounted)
+        busybox umount -l /system
 
         exit 0
     fi
 
     # if a omni.cfg exists, then this is a first time install
-    # let's format the volumes and restore radio and efs
+    # let's format the volumes and restore modem and efs
 
     # remove the omni.cfg to prevent this from looping
     busybox rm -f /sdcard/omni.cfg
 
-    # setup lvm volumes
-    /lvm/sbin/lvm pvcreate $MMC_PART
-    /lvm/sbin/lvm vgcreate lvpool $MMC_PART
-    format_partitions
+    # unmount and format system (recovery seems to expect system to be unmounted)
+    # unmount and format data
+    busybox umount -l /data
+    busybox umount -l /system
+
+    /tmp/make_ext4fs -b 4096 -g 32768 -i 8192 -I 256 -a /data /dev/block/mmcblk0p2
+    /tmp/erase_image system
 
     # restart into recovery so the user can install further packages before booting
     busybox touch /cache/.startrecovery
 
-    if busybox test -e /sdcard/omni.mtdupd ; then
-        # this is an upgrade with changed MTD mapping for /data, /cache, /system
-        # so return to updater-script after formatting them
-
-        busybox rm -f /sdcard/omni.mtdupd
-
-        exit 0
-    fi
-
     if $IS_GSM ; then
         # restore efs backup
-        if busybox test -e /sdcard/backup/efs/nv_data.bin || \
-                busybox test -e /sdcard/backup/efs/root/afs/settings/nv_data.bin ; then
+        if busybox test -e /sdcard/backup/efs/nv_data.bin ; then
             busybox umount -l /efs
             /tmp/erase_image efs
             busybox mkdir -p /efs
 
             if ! busybox grep -q /efs /proc/mounts ; then
-                if ! busybox mount -t yaffs2 /dev/block/mtdblock4 /efs ; then
+                if ! busybox mount -t yaffs2 /dev/block/$EFS_PART /efs ; then
                     busybox echo "Cannot mount efs."
                     exit 6
                 fi
@@ -326,4 +291,3 @@ elif busybox test -e /dev/block/mtdblock0 ; then
 
     exit 0
 fi
-
