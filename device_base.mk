7ab56cca277ecd660126a0aba065ea4efe1969a1
Humberto Borba <humberos@gmail.com>
2014-05-01 01:21:04 +0200

    p1-common: add zram init
    This reverts commit b6548361547f46be7d7b3f70008002a27c73c604.
    
    Change-Id: I9426a0bffa667e78e876ad5ec613f3cdd2c0e17c

--- device_base.mk
+++ device_base.mk
@@ -223,7 +223,8 @@ PRODUCT_COPY_FILES += \
 
 # init.d scripts
 PRODUCT_COPY_FILES += \
-    device/samsung/p1-common/prebuilt/etc/init.d/00random:system/etc/init.d/00random
+    device/samsung/p1-common/prebuilt/etc/init.d/00random:system/etc/init.d/00random \
+    device/samsung/p1-common/prebuilt/etc/init.d/99zram:system/etc/init.d/99zram
 
 # bml_over_mtd
 PRODUCT_COPY_FILES += \
