970d865f69f2bd82f4bb9b3fe802bbd6ead265bf
Humberto Borba <humberos@gmail.com>
2014-12-06 22:09:32 -0200

    p1-common: bringup android-5.0
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: Ifda4f38e42d108eb2dbb66dd892a28053fff498b

--- DeviceSettings/src/org/omnirom/device/DeviceSettings.java
+++ DeviceSettings/src/org/omnirom/device/DeviceSettings.java
@@ -74,8 +74,10 @@ public class DeviceSettings extends Activity {
                 ButtonFragmentActivity.class, null);
         mTabsAdapter.addTab(bar.newTab().setText(R.string.category_radio_title),
                 RadioFragmentActivity.class, null);
+        /*
         mTabsAdapter.addTab(bar.newTab().setText(R.string.category_tvout_title),
                 TVFragmentActivity.class, null);
+        */
         mTabsAdapter.addTab(bar.newTab().setText(R.string.category_vibrator_tuning_title),
                 VibratorFragmentActivity.class, null);
         if (savedInstanceState != null) {
