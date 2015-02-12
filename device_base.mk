00eb1c1df8b0d5e784127072a77dcd0c853d33b2
ferox <frhatfrat@gmail.com>
2015-02-12 19:36:52 -0200

    p1-common: add limits for background apps
    also, remove unused properties
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: Ic9f2847019c3c0dab3b2a8bd0cffcab9f6f95284

--- device_base.mk
+++ device_base.mk
@@ -104,10 +104,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.networklocation=1
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.kernel.android.checkjni=0 \
-    dalvik.vm.checkjni=false \
-    dalvik.vm.debug.alloc=0 \
-    dalvik.vm.dexopt-data-only=1 \
+    ro.sys.fw.bg_apps_limit=16 \
+    ro.config.max_starting_bg=10 \
     dalvik.vm.jit.codecachesize=0
 
 # Set default property
